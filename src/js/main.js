0c848deaf68b79ceb4a11b5066d4b3ed6c400de4
RomanKashik <kashik.roman@rambler.ru>
2020-06-05 18:49:24 +0300

    header slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
     $('.slider').slick({
         vertical: true,
-        verticalSwiping:true
+        verticalSwiping:true,
+        arrows:false,
+        dots: true,
+        // dotsClass:'slider__dots',
+        appendDots:$('.slider__dots'),
+    });
+
+    $('.btn__search').on('click',function (e) {
+        e.preventDefault();
+
+        $('.input__search').addClass('show');
+
+        if ( $('.input__search').hasClass('show')) {
+            $('.btn__search').off();
+        }
     });
 });
